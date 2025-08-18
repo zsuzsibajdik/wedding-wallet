5e63d88d87eea4f666caba04fdcb4728c0beed56
Bajdik Zsuzsanna <zsuzsibajdik@gmail.com>
2025-08-18 11:44:06 +0200

    structure done, react and firebase done

---
+++ wedding-wallet/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+//const firebaseURL = 'https://wedding-wallet-codecool-default-rtdb.europe-west1.firebasedatabase.app/'
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
