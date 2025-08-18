8a6373dba06f7415b96698e760e41f5f3cd0448b
Bajdik Zsuzsanna <zsuzsibajdik@gmail.com>
2025-08-18 14:35:03 +0200

    navbar done, css needed

--- wedding-wallet/src/App.jsx
+++ wedding-wallet/src/App.jsx
@@ -1,13 +1,28 @@
-
+import { Navbar } from './components/Navbar'
+import { useState } from 'react';
 import './App.css'
 
 //const firebaseURL = 'https://wedding-wallet-codecool-default-rtdb.europe-west1.firebasedatabase.app/'
 function App() {
-  
+  const pages = ({homepage: '<Homepage/>', vendorpage: '<Vendorpage/>', calculatepage: '<Calculator/>', todopage: '<Todopage/>'});
+  const [actualpage, setActualpage] = useState('homepage')
+  function loadHomePage(){
+    setActualpage('homepage');
+  }
+  function loadVendorsPage(){
+    setActualpage('vendorpage');
+  }
+  function loadCalculator(){
+    setActualpage('calculatepage');
+  }
+  function loadTodoPage(){
+    setActualpage('todopage');
+  }
 
   return (
     <>
-      
+      <Navbar onHome={loadHomePage} onVendor={loadVendorsPage} onCalculate={loadCalculator} onTodo={loadTodoPage}/>
+      {pages[actualpage]};
     </>
   )
 }
---
+++ wedding-wallet/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+export function Navbar({onHome, onVendor, onCalculate, onTodo}){
+    return(
+        <>
+            <nav className="navbar">
+                <div className="navbar-left">
+                    <a href="#" onClick={onHome} className="logo">
+                    The Wedding Wallet
+                    </a>
+                </div>
+                <div className="navbar-center">
+                    <ul className="nav-links">
+                    <li>
+                        <a href="#" className="navbutton" onClick={onVendor}>Vendors</a>
+                    </li>
+                    <li>
+                        <a href="#" className="navbutton" onClick={onCalculate}>Expense calculator</a>
+                    </li>
+                    <li>
+                        <a href="#" className="navbutton" onClick={onTodo}>Tasks</a>
+                    </li>
+                    </ul>
+                </div>
+                <div className="navbar-right">
+                    <span>Username</span>
+                </div>
+            </nav>
+        </>
+    )
+}
