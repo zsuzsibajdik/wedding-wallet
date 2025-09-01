44fec6201cabf7e0bbcdf7eea785f19b30403674
sestak817 <sestakrp@gmail.com>
2025-09-01 19:36:55 +0200

    implement-basic-routing-with-navbar-links

--- wedding-wallet/src/App.jsx
+++ wedding-wallet/src/App.jsx
@@ -1,47 +1,30 @@
-import { Navbar } from './components/Navbar'
-import Calculator from './components/Calculatorpage';
-import { Homepage } from './components/Homepage';
-import { Footer } from './components/Footer';
-import { useState } from 'react';
-import './App.css'
-import Vendorpage from './components/Vendorpage';
-import TodoList from './components/Todo/TodoList';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Calculator from "./components/Calculatorpage";
+import Homepage from "./components/Homepage";
+import Footer from "./components/Footer";
+import Vendorpage from "./components/Vendorpage";
+import TodoList from "./components/Todo/TodoList";
+import "./App.css";
 
-//const firebaseURL = 'https://wedding-wallet-codecool-default-rtdb.europe-west1.firebasedatabase.app/'
 function App() {
-
-  const pages = {
-    homepage: <Homepage/>,
-    vendorpage: <Vendorpage />,
-    calculatepage: <Calculator/>,
-    todopage: <TodoList/>
-  };
-  
-  const [actualpage, setActualpage] = useState('homepage')
-  function loadHomePage(){
-    setActualpage('homepage');
-  }
-  function loadVendorsPage(){
-    setActualpage('vendorpage');
-  }
-  function loadCalculator(){
-    setActualpage('calculatepage');
-  }
-  function loadTodoPage(){
-    setActualpage('todopage');
-  }
-
-return (
-  <div className="app-shell">
-    <Navbar
-      onHome={loadHomePage}
-      onVendor={loadVendorsPage}
-      onCalculate={loadCalculator}
-      onTodo={loadTodoPage}
-    />
-    <main className="app-main">{pages[actualpage]}</main>
-    <Footer />
-  </div>
-);
+  return (
+    <Router>
+      <div className="app-shell">
+        <Navbar />
+        <main className="app-main">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/vendors" element={<Vendorpage />} />
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/todos" element={<TodoList />} />
+            <Route path="*" element={<h2 style={{padding:20}}>Page not found</h2>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
+
 export default App;
--- wedding-wallet/src/components/Navbar.jsx
+++ wedding-wallet/src/components/Navbar.jsx
@@ -1,33 +1,41 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-export function Navbar({onHome, onVendor, onCalculate, onTodo}){
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-    return(
-        <>
-            <nav className="navbar">
-                <div className="navbar-left">
-                    <a href="#" onClick={onHome} className="logo">
-                    The Wedding Wallet
-                    </a>
-                </div>
-                <div className="menu-toggle" onClick={toggleMenu}>
-                    ☰
-                </div>
-                <div className={`navbar-center ${isMenuOpen ? "active" : ""}`}>
-                    <ul className="nav-links">
-                    <li>
-                        <a href="#" className="navbutton" onClick={onVendor}>Vendors</a>
-                    </li>
-                    <li>
-                        <a href="#" className="navbutton" onClick={onCalculate}>Expense calculator</a>
-                    </li>
-                    <li>
-                        <a href="#" className="navbutton" onClick={onTodo}>Tasks</a>
-                    </li>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    )
+export function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <NavLink to="/" className="logo" onClick={() => setIsMenuOpen(false)}>
+          The Wedding Wallet
+        </NavLink>
+      </div>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </div>
+      <div className={`navbar-center ${isMenuOpen ? "active" : ""}`}>
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/vendors" className="navbutton" onClick={() => setIsMenuOpen(false)}>
+              Vendors
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/calculator" className="navbutton" onClick={() => setIsMenuOpen(false)}>
+              Expense calculator
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/todos" className="navbutton" onClick={() => setIsMenuOpen(false)}>
+              Tasks
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
+
+export default Navbar;
